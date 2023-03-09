90c9d0fa7420cec2b55017fc99a848db8f2d1cd7
Nathan D Strzelczyk <truenathand@gmail.com>
2023-03-08 22:12:40 -0600

    Changed repo, updated for new purpose

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# quote-template-generator
+###### estimated-net-worth-calculator
+
+<h1 align="center">My Estimated Net Worth Calculator</h1>
+
+This estimates my net worth in dollars and tears, adding $300 to the total each and every week, which will make me feel slightly better about my life choices.
+
+<p align="center">
+  <img src="" alt="Picture: My Estimated Net Worth Calculator">
+  <br>
+  <sub>Picture: My Estimated Net Worth Calculator</sub>
+</p>
+
--- js/main.js
+++ js/main.js
@@ -1,2 +1,60 @@
 console.log("Hello, World.");
 
+
+// Defines the starting total of $124,000 as of March 8th, 2023 (03/08/23), and the starting date to begin adding money on
+let estimatedNetWorth = 124000;
+let startDate = new Date('2023-03-08');
+
+// Defines the amount of money to add on Wednesday
+const addedEveryWednesday = 300;
+
+// Gets the current date
+let currentDate = new Date();
+
+// Calculates the number of Wednesdays that have passed since the starting date
+let weeksElapsed = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24 * 7));
+
+// Calculates the total amount to add based on the number of Wednesdays that have passed
+let amountToAdd = weeksElapsed * addedEveryWednesday;
+
+// Adds the calculated amount to the starting total
+estimatedNetWorth += amountToAdd;
+
+// Formats the estimated net worth with commas using the toLocaleString() method
+let formattedNetWorth = estimatedNetWorth.toLocaleString();
+
+// Outputs the result to an HTML div
+document.getElementById("estimatedNetWorth").innerHTML = `$ ${formattedNetWorth}`;
+
+
+// FOOTER CODE
+// Gets the element with the id footer
+let footer = document.getElementById('footer');
+
+// Gets the last modified date
+let lastModified = new Date(document.lastModified);
+
+// Gets the year
+let year = lastModified.getFullYear();
+
+// Gets the month
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = months[lastModified.getMonth()];
+
+// Gets the day
+let day = lastModified.getDate();
+// Finds and gets the suffix of the day
+let daySuffix;
+if (day % 10 == 1) {
+    daySuffix = "st";
+} else if (day % 10 == 2) {
+    daySuffix = "nd";
+} else if (day % 10 == 3) {
+    daySuffix = "rd";
+} else {
+    daySuffix = "th";
+}
+
+// Defines the footer text
+footer.innerText = "Last modified: " + month + " " + day + daySuffix + ", " + year;
+
